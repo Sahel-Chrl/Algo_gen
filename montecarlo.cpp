667d444841b27afcc0b612a98d3dae84055b5f6e
Sahel-Chrl <sahel.charlat@gmail.com>
2026-01-19 23:08:09 +0100

    montecalo

--- montecarlo.cpp
+++ montecarlo.cpp
@@ -2,25 +2,28 @@
 using namespace std;
 #include <random>
 
+// exemple methode de montecarlo; approximation de pi
+
+
 float hasard () {
     return (float)rand()/RAND_MAX;
-}
+} //fonction alea entre 0,1
 
 int main () {
     cout<< "Bonjour!!" <<endl;
     float x,y;
     int n,m;
-    cout << "donne un entier" << endl;
+    cout << "Donne un entier" << endl;
     cin >> n;
     m=0;
     for (int i=1 ; i<=n ; i++ ) {
         x=hasard();
-        y=hasard();
+        y=hasard(); // appel de la fct
         if (x*x+y*y<=1) {
             m++;
-            //point dans le cercle
+            //ajout d'un point dans le cercle
         }
     }
-    cout << 4.*m/n << endl;
+    cout << 4.*m/n << endl; // calcul de pi
     return 0;
 }
