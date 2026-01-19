ca154464fd954c8b3ff2465613625577d21614e9
Sahel Charlat <sahelcharlat@dhcp-154-16.ens-lyon.fr>
2026-01-19 18:58:51 +0100

    montecarlo

---
+++ montecarlo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include <random>
+
+float hasard () {
+    return (float)rand()/RAND_MAX;
+}
+
+int main () {
+    cout<< "Bonjour" <<endl;
+    float x,y;
+    int n,m;
+    cout << "donne un entier" << endl;
+    cin >> n;
+    m=0;
+    for (int i=1 ; i<=n ; i++ ) {
+        x=hasard();
+        y=hasard();
+        if (x*x+y*y<=1) {
+            m++;
+        }
+    }
+    cout << 4.*m/n << endl;
+    return 0;
+}
