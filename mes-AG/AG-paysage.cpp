b483c6f93f8d1ca5c059c18f3fb39e32196f1c8f
Sahel-Chrl <sahel.charlat@gmail.com>
2026-02-18 04:38:54 +0100

    Premier main

--- mes-AG/AG-paysage.cpp
+++ mes-AG/AG-paysage.cpp
@@ -1,1 +1,16 @@
 #include "Paysage.h"
+
+int maint ()
+{
+    Paysage p;
+
+    int nbCercles;
+    cout << "combien de cercles dans ton paysage ?"<<endl;
+    cin >>nbCercles;
+
+    auto centres = p.createCenter(nbCercles);
+    auto rayons = p.createRayons(nbCercles);
+
+    p.afficheCerlces(centres,rayons,nbCercles);
+    return 0;
+}
